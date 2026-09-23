479c7ebedf9496318f8690919d05f7a89209cb9a
Artemis Weber <wliersa15@gmail.com>
2026-09-23 08:36:48 -0600

    course initialization complete

--- src/init_db.py
+++ src/init_db.py
@@ -9,11 +9,18 @@ from app import app, db
 from app.models import Course
 
 # TODO
-courses = [
-    
+courses = [ 
+    ("CS","3250","SW Dev Methods and Tools",4),
+    ("GWS","1200","Sexuality, Race, and Power",3),
+    ("JMP","2610","Intro to Technical Writing",3),
+    ("MTH","3130","Applied Methods in Linear Algebra",4),
+    ("PHI","3370","Computers, Ethics, and Society",3)
 ]
+
 
 with app.app_context():
     for prefix, number, name, credits in courses:
-        pass
+        new_course = Course(prefix=prefix, number=number, name=name, credits=credits)
+        db.session.add(new_course)
+    db.session.commit()
     print(f'Loaded {len(courses)} courses.')
